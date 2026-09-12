e7d4f776633f203fedcd6da636971d35499daff0
lcmasenya <chamaine.masenya@gmail.com>
2026-09-12 12:20:24 +0200

    restore

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name? ");
+        String first = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        String last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
+
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.WriteLine("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        string letter = "";
+
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations! You Passed the course!");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry, try next time.");
+        }
+
     }
 }
